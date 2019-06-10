8cf4614f5024bfa93509ea1c3853a2b31354181e
Renato Alves <alves.rjc@gmail.com>
2019-06-10 21:20:52 +0200

    ENH Rename singularity recipes following guidelines
    from https://github.com/singularityhub/singularityhub.github.io/wiki/Build-A-Container#naming-recipes

--- containers/build_and_push.sh
+++ containers/build_and_push.sh
@@ -3,13 +3,13 @@
 TAG="1.0.0"
 
 for name in ngless ng-meta-dog-gut ng-meta-pig-gut ng-meta-mouse-gut ng-meta-human-gut ng-meta-marine ; do
-       docker-compose build $name
-       docker push nglesstoolkit/$name:$TAG &
+    docker-compose build $name
+    docker push nglesstoolkit/$name:$TAG &
 done
 
-for recipe in Singularity-ng* ; do
-    TARGET="$(echo "$recipe" | cut -d- -f2 -)"
-    singularity build "${TARGET}.simg" "$recipe"
+for recipe in singularity/*/* ; do
+    TARGET="$(echo "$recipe" | cut -d/ -f2 -)"
+    singularity build "ng-meta-${TARGET}.simg" "$recipe"
 done
 
 wait
